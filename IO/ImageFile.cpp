ff5cab75eef56986590b6d8992694634e0f2d83a
Pavel Krajcevski <pavel@cs.unc.edu>
2012-08-26 16:37:10 -0400

    Added a few more structural preparations

--- IO/ImageFile.cpp
+++ IO/ImageFile.cpp
@@ -1,28 +1,48 @@
 #include "ImageFile.h"
 
+ImageFile::ImageFile(const char *filename) : 
+  m_PixelData(0)
+{
+  unsigned char *rawData = ReadFileData(filename);
+  DetectFileFormat(filename);
+  LoadImage(rawData);
+  delete [] rawData;
+}
+
+ImageFile::ImageFile(const char *filename, EImageFileFormat format) :
+  m_FileFormat(format),
+  m_PixelData(0)
+{
+  unsigned char *rawData = ReadFileData(filename);
+  LoadImage(rawData);
+  delete [] rawData;
+}
+
+ImageFile::~ImageFile() {
+  if(m_PixelData) {
+    delete [] m_PixelData;
+  }
+}
+
+void ImageFile::GetPixels() const {
+
+}
+
+EImageFileFormat ImageFile::DetectFileFormat() {
+}
+
+void ImageFile::LoadImage(const unsigend char *rawImageData) {
+}
+
+void ImageFile::LoadPNGImage(const unsigned char *rawImageData) {
+}
+
 #ifdef _MSC_VER
-ImageFile::ImageFile(const char *filename) {
-}
-
-ImageFile::~ImageFile() {
-}
-
-void ImageFile::GetPixels() const {
+unsigned char *ImageFile::ReadFileData(const char *filename) {
 
 }
-
 #else
-#include <stdlib.h>
-
-ImageFile::ImageFile(const char *filename) {
-}
-
-ImageFile::~ImageFile() {
-}
-
-void ImageFile::GetPixels() const {
+unsigned char *ImageFile::ReadFileData(const char *filename) {
 
 }
-
 #endif
-
--- IO/ImageFile.h
+++ IO/ImageFile.h
@@ -1,21 +1,36 @@
 #ifedef _IMAGE_FILE_H_ 
 #define _IMAGE_FILE_H_ 
 
+enum EImageFileFormat {
+  eFileFormat_PNG,
+
+  kNumImageFileFormats
+};
+
 class ImageFile {
 
 public:
 
   ImageFile(const char *filename);
+  ImageFile(const char *filename, EImageFileFormat format);
   ~ImageFile();
 
   void GetWidth() const { return m_Width; }
   void GetHeight() const { return m_Height; }
-  void GetPixels() const;
+  const unsigned char *GetPixels() const { return m_PixelData; }
 
  private:
   unsigned int m_Handle;
   unsigned int m_Width;
   unsigned int m_Height;
+  unsigned char *m_PixelData;
+  const EImageFileFormat m_FileFormat;
+
+  static unsigned char *ReadFileData(const char *filename);
+  static EFileFormat DetectFileFormat(const char *filename);
+
+  bool LoadImage(const unsigned char *rawImageData);
+  bool LoadPNGImage(const unsigned char *rawImageData);
 };
 
 
