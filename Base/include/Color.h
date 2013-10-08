2159a6688a5b56f425d9894c66c8b4013da9bc00
Pavel Krajcevski <pavel@cs.unc.edu>
2013-10-08 15:07:43 -0400

    Add colors.

---
+++ Base/include/Color.h
@@ -0,0 +1,100 @@
+/* FasTC
+ * Copyright (c) 2013 University of North Carolina at Chapel Hill.
+ * All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for educational, research, and non-profit purposes, without
+ * fee, and without a written agreement is hereby granted, provided that the
+ * above copyright notice, this paragraph, and the following four paragraphs
+ * appear in all copies.
+ *
+ * Permission to incorporate this software into commercial products may be
+ * obtained by contacting the authors or the Office of Technology Development
+ * at the University of North Carolina at Chapel Hill <otd@unc.edu>.
+ *
+ * This software program and documentation are copyrighted by the University of
+ * North Carolina at Chapel Hill. The software program and documentation are
+ * supplied "as is," without any accompanying services from the University of
+ * North Carolina at Chapel Hill or the authors. The University of North
+ * Carolina at Chapel Hill and the authors do not warrant that the operation of
+ * the program will be uninterrupted or error-free. The end-user understands
+ * that the program was developed for research purposes and is advised not to
+ * rely exclusively on the program for any reason.
+ *
+ * IN NO EVENT SHALL THE UNIVERSITY OF NORTH CAROLINA AT CHAPEL HILL OR THE
+ * AUTHORS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL,
+ * OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF
+ * THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF NORTH CAROLINA
+ * AT CHAPEL HILL OR THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ *
+ * THE UNIVERSITY OF NORTH CAROLINA AT CHAPEL HILL AND THE AUTHORS SPECIFICALLY
+ * DISCLAIM ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE AND ANY 
+ * STATUTORY WARRANTY OF NON-INFRINGEMENT. THE SOFTWARE PROVIDED HEREUNDER IS ON
+ * AN "AS IS" BASIS, AND THE UNIVERSITY  OF NORTH CAROLINA AT CHAPEL HILL AND
+ * THE AUTHORS HAVE NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, 
+ * ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ * Please send all BUG REPORTS to <pavel@cs.unc.edu>.
+ *
+ * The authors may be contacted via:
+ *
+ * Pavel Krajcevski
+ * Dept of Computer Science
+ * 201 S Columbia St
+ * Frederick P. Brooks, Jr. Computer Science Bldg
+ * Chapel Hill, NC 27599-3175
+ * USA
+ * 
+ * <http://gamma.cs.unc.edu/FasTC/>
+ */
+
+#ifndef BASE_INCLUDE_COLOR_H_
+#define BASE_INCLUDE_COLOR_H_
+
+#include "TexCompTypes.h"
+#include "Vector4.h"
+
+namespace FasTC {
+
+class Color : public Vec4f {
+ public:
+  Color(float r, float g, float b, float a) : Vec4f(a, r, g, b) { }
+  Color() : Vec4f(0, 0, 0, 0) { }
+
+  // Let's allow us to use the operators...
+  template<typename T>
+  Color &operator=(const Vector4<T> &other) {
+    Vec4f::operator=(other);
+    return *this;
+  }
+
+  template<typename T>
+  Color(const Vector4<T> &other) : Vec4f(other) { }
+  
+  const float &A() const { return vec[0]; }
+  float &A() { return vec[0]; }
+  const float &R() const { return vec[1]; }
+  float &R() { return vec[1]; }
+  const float &G() const { return vec[2]; }
+  float &G() { return vec[2]; }
+  const float &B() const { return vec[3]; }
+  float &B() { return vec[3]; }
+  const float &Component(uint32 idx) const { return vec[idx]; }
+  float &Component(uint32 idx) { return vec[idx]; }
+
+  // Take all of the components, transform them to their 8-bit variants,
+  // and then pack each channel into an R8G8B8A8 32-bit integer. We assume
+  // that the architecture is little-endian, so the alpha channel will end
+  // up in the most-significant byte.
+  uint32 Pack() const;
+  void Unpack(uint32 rgba);
+
+  // Tests for equality by comparing the values and the bit depths.
+  bool operator==(const Color &) const;
+};
+
+}  // namespace FasTC
+
+#endif  // BASE_INCLUDE_COLOR_H_
