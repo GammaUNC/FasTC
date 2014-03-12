e96cecc13aa0eec97d37054deff53d7b52e694b9
Pavel Krajcevski <pavel@cs.unc.edu>
2014-03-12 03:01:28 -0400

    Add some typename specifiers

--- Base/include/VectorBase.h
+++ Base/include/VectorBase.h
@@ -110,7 +110,7 @@ namespace FasTC {
                                              const VectorTypeTwo &v2) {
     VectorTypeOne a(v1);
     for(int i = 0; i < VectorTypeOne::Size; i++) {
-      a(i) += static_cast<VectorTypeOne::ScalarType>(v2[i]);
+      a(i) += static_cast<typename VectorTypeOne::ScalarType>(v2[i]);
     }
     return a;
   }
@@ -132,7 +132,7 @@ namespace FasTC {
                                                 const VectorTypeTwo &v2) {
     VectorTypeOne a(v1);
     for(int i = 0; i < VectorTypeOne::Size; i++) {
-      a(i) -= static_cast<VectorTypeOne::ScalarType>(v2[i]);
+      a(i) -= static_cast<typename VectorTypeOne::ScalarType>(v2[i]);
     }
     return a;
   }
