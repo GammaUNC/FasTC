840ae71cbc34aaae4f1bef699de6cd4e9ad4b536
Pavel Krajcevski <pavel@cs.unc.edu>
2014-03-18 19:37:42 -0400

    Add small tool that decompresses an image.

---
+++ CLTool/src/decomp.cpp
@@ -0,0 +1,85 @@
+/* FasTC
+ * Copyright (c) 2012 University of North Carolina at Chapel Hill. All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software and its documentation for educational, 
+ * research, and non-profit purposes, without fee, and without a written agreement is hereby granted, 
+ * provided that the above copyright notice, this paragraph, and the following four paragraphs appear 
+ * in all copies.
+ *
+ * Permission to incorporate this software into commercial products may be obtained by contacting the 
+ * authors or the Office of Technology Development at the University of North Carolina at Chapel Hill <otd@unc.edu>.
+ *
+ * This software program and documentation are copyrighted by the University of North Carolina at Chapel Hill. 
+ * The software program and documentation are supplied "as is," without any accompanying services from the 
+ * University of North Carolina at Chapel Hill or the authors. The University of North Carolina at Chapel Hill 
+ * and the authors do not warrant that the operation of the program will be uninterrupted or error-free. The 
+ * end-user understands that the program was developed for research purposes and is advised not to rely 
+ * exclusively on the program for any reason.
+ *
+ * IN NO EVENT SHALL THE UNIVERSITY OF NORTH CAROLINA AT CHAPEL HILL OR THE AUTHORS BE LIABLE TO ANY PARTY FOR 
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF THE 
+ * USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF NORTH CAROLINA AT CHAPEL HILL OR THE 
+ * AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THE UNIVERSITY OF NORTH CAROLINA AT CHAPEL HILL AND THE AUTHORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING, 
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE AND ANY 
+ * STATUTORY WARRANTY OF NON-INFRINGEMENT. THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY 
+ * OF NORTH CAROLINA AT CHAPEL HILL AND THE AUTHORS HAVE NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, 
+ * ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ * Please send all BUG REPORTS to <pavel@cs.unc.edu>.
+ *
+ * The authors may be contacted via:
+ *
+ * Pavel Krajcevski
+ * Dept of Computer Science
+ * 201 S Columbia St
+ * Frederick P. Brooks, Jr. Computer Science Bldg
+ * Chapel Hill, NC 27599-3175
+ * USA
+ * 
+ * <http://gamma.cs.unc.edu/FasTC/>
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cassert>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#ifdef _MSC_VER
+#  include <SDKDDKVer.h>
+#  include <Windows.h>
+#endif
+
+#include "Image.h"
+#include "ImageFile.h"
+#include "TexComp.h"
+#include "ThreadSafeStreambuf.h"
+
+void PrintUsage() {
+  fprintf(stderr, "Usage: decomp <in_img> <out_img>\n");
+  fprintf(stderr, "\tIf in_img is not a compressed image, then this tool simply copies the image.\n");
+}
+
+int main(int argc, char **argv) {
+  if(argc != 3) {
+    return 1;
+  }
+
+  ImageFile imgf (argv[1]);
+  if(!imgf.Load()) {
+    fprintf(stderr, "Error loading file: %s\n", argv[1]);
+    return 1;
+  }
+
+  FasTC::Image<> *img = imgf.GetImage();
+
+  EImageFileFormat fmt = ImageFile::DetectFileFormat(argv[2]);
+  ImageFile cImgFile (argv[2], fmt, *img);
+  cImgFile.Write();
+
+  return 0;
+}
